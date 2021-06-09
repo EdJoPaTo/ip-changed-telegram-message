48813307daba149dcdaf4deb283a35afb0ab7d20
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-06-09 10:54:58 +0200

    feat(notifier): improve formatting
    both console and tg message

--- src/notifier.rs
+++ src/notifier.rs
@@ -4,7 +4,7 @@ use teloxide::{
     payloads::SendMessageSetters,
     prelude::{Request, Requester},
     types::ParseMode,
-    utils::html::code_block,
+    utils::html,
     Bot,
 };
 
@@ -23,17 +23,17 @@ impl Notifier {
     }
 
     pub async fn notify_startup(&self, ips: &IPs) -> anyhow::Result<()> {
+        println!("IPv4: {:?}", ips.v4);
+        println!("IPv6: {:?}", ips.v6);
         let mut lines = Vec::new();
         if let Some(ip) = &ips.v4 {
-            lines.push(format!("IPv4: {}", ip));
+            lines.push(format!("IPv4: {}", html::code_inline(ip)));
         }
         if let Some(ip) = &ips.v6 {
-            lines.push(format!("IPv6: {}", ip));
+            lines.push(format!("IPv6: {}", html::code_inline(ip)));
         }
         let text = lines.join("\n");
-        println!("{}", text);
-
-        let text = format!("Bot startup done. IPs at startup:\n{}", code_block(&text));
+        let text = format!("Bot startup done. IPs at startup:\n{}", text);
         self.bot
             .send_message(self.target_chat, &text)
             .disable_notification(true)
@@ -51,31 +51,51 @@ impl Notifier {
         maximum_down_duration: &Duration,
     ) -> anyhow::Result<()> {
         let mut lines = Vec::new();
-        lines.push(format!(
+
+        let downtime = format!(
             "Network was down for up to {} seconds = {:.1} minutes.",
             maximum_down_duration.as_secs(),
             maximum_down_duration.as_secs_f32() / 60.0,
-        ));
+        );
+        println!("{}", downtime);
+        lines.push(downtime);
+
         if old.v4 != new.v4 {
+            println!("IPv4 old: {:?}", old.v4);
+            println!("IPv4 new: {:?}", new.v4);
+            lines.push(html::bold("IPv4"));
+            let mut line = String::new();
             if let Some(ip) = &old.v4 {
-                lines.push(format!("IPv4 old: {}", ip));
+                line += &html::code_inline(&ip);
+            } else {
+                line += "None";
             }
+            line += " \u{2192} "; // →
             if let Some(ip) = &new.v4 {
-                lines.push(format!("IPv4 new: {}", ip));
+                line += &html::code_inline(&ip);
+            } else {
+                line += "None";
             }
+            lines.push(line);
         }
         if old.v6 != new.v6 {
+            println!("IPv6 old: {:?}", old.v6);
+            println!("IPv6 new: {:?}", new.v6);
+            lines.push(html::bold("IPv6"));
             if let Some(ip) = &old.v6 {
-                lines.push(format!("IPv6 old: {}", ip));
+                lines.push(html::code_inline(&ip));
+            } else {
+                lines.push("None".to_string())
             }
+            lines.push("\u{2193}".to_string()); // ↓
             if let Some(ip) = &new.v6 {
-                lines.push(format!("IPv6 new: {}", ip));
+                lines.push(html::code_inline(&ip));
+            } else {
+                lines.push("None".to_string())
             }
         }
-        let text = lines.join("\n");
-        println!("{}", text);
 
-        let text = code_block(&text);
+        let text = lines.join("\n");
         self.bot
             .send_message(self.target_chat, &text)
             .disable_web_page_preview(true)
