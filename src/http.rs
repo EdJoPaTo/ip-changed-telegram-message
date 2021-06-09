d92c187ffc499a2cf91e0950a68d8266a089a70b
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-06-09 10:19:24 +0200

    feat(http): retry get reqwest

--- src/http.rs
+++ src/http.rs
@@ -19,22 +19,31 @@ impl Http {
     pub fn new() -> Self {
         Self {
             client: ClientBuilder::new()
-                .timeout(Duration::from_secs(5))
+                .timeout(Duration::from_secs(2))
                 .user_agent(USER_AGENT)
                 .build()
                 .expect("failed to create reqwest client"),
         }
     }
 
-    pub async fn get(&self, url: &str) -> anyhow::Result<String> {
-        let body = self
-            .client
+    async fn get_once(&self, url: &str) -> reqwest::Result<String> {
+        self.client
             .get(url)
             .send()
             .await?
             .error_for_status()?
             .text()
-            .await?;
-        Ok(body)
+            .await
+    }
+
+    pub async fn get(&self, url: &str) -> reqwest::Result<String> {
+        let mut i: usize = 1;
+        loop {
+            let body = self.get_once(url).await;
+            if i >= 2 || body.is_ok() {
+                return body;
+            }
+            i += 1;
+        }
     }
 }
